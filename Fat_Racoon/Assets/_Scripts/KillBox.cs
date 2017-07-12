8da72e07492490942592c65850b802d070ad46e1
smaguire <maguire.sean037@gmail.com>
2017-07-12 14:03:06 -0700

    KillBox
    KILLLLLLL

---
+++ Fat_Racoon/Assets/_Scripts/KillBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillBox : MonoBehaviour {
+
+    Scene scene;
+    GameObject camera;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        camera = GameObject.Find("MainCamera");
+    }
+	
+	void OnTriggerEnter2D(Collider2D other)
+    {
+      
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(camera);
+            SceneManager.LoadScene(scene.name);
+        }
+
+        Destroy(other.gameObject);
+
+    }
+}
