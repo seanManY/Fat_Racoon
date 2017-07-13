9a0b53aa325b14734a3e12cd2969bbba971208b6
smaguire <maguire.sean037@gmail.com>
2017-07-13 10:52:26 -0700

    fixed the fruits
    can pick up foots and is added to weight

---
+++ Fat_Racoon/Assets/_Scripts/Fruit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+
+    string fruitName;
+    float calories;
+    bool collectedFruit = false;
+
+	// Use this for initialization
+	void Start () {
+        calories = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void createFruit(string name, float calories)
+    {
+        fruitName = name;
+        this.calories = calories;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+
+        if (other.gameObject.tag == "FoodCollector")
+        {
+            collectedFruit = true;
+            Racoon racoon = other.GetComponentInParent<Racoon>();
+            racoon.AddCalories(calories);
+            Debug.Log(racoon.getWeight());
+            Destroy(this.gameObject);            
+        }
+        
+
+    }
+}
---
+++ Fat_Racoon/Assets/_Scripts/Racoon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racoon : MonoBehaviour {
+
+    public int   health = 3;
+    public float weight = 5.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float getWeight()
+    {
+        return weight;
+    }
+
+    public void AddCalories (float weight)
+    {
+        this.weight += weight;
+    }
+}
